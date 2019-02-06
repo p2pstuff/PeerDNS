727faeb85c6b3ef9f7b140a78109977b340f319d
Alex Auvolat <alex.auvolat-bernstein@inria.fr>
2019-02-06 18:03:09 +0100

    Config file example

--- config/config.exs
+++ config/config.exs
@@ -1,28 +1,93 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# third-party users, it should be done in your "mix.exs" file.
 
-# You can configure your application as:
-#
-#     config :peerdns, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:peerdns, :key)
-#
-# You can also configure a third-party app:
-#
+# Set info logging level (optionnal)
 config :logger, level: :info
 
-config :peerdns, tld: [".peer", ".mesh", ".h", ".hype", ".fc00"]
-config :peerdns, other_proxy: {"208.67.222.222", 53}
 
+# Standard value, do not change this!
+standard_api_port = 14123
+config :peerdns, standard_api_port: standard_api_port
+
+# Some trusted peer IPs
+# Replace with your own here and when they are used later
+peer_fly11 = "fc18:e736:105d:d49a:2ab5:14a2:698f:7021"
+
+
+# The TLDs for which PeerDNS will handle naming
+config :peerdns, tld: [".peer", ".mesh", ".h", ".hype", ".fc00"]
+
+# For other TLDs, DNS requests are proxied to this DNS server
+config :peerdns, outside: {"208.67.222.222", 53}
+
+# Where to listen for DNS requests
+config :peerdns, listen_dns: [
+  # Use either the first two or the second two
+
+  # {"0.0.0.0", 53}   # Open DNS server on IPv4 network
+  # {"::", 53}        # Open DNS server on IPv6 network
+
+  {"127.0.0.1", 53} # Private DNS server on IPv4 network
+  {"::1", 53}       # Private DNS server on IPv6 network
+]
+
+# Where to listen for PeerDNS API requests
+# The PeerDNS API is used by other PeerDNS instances to exchange data with us.
+# Recommended: do not change this, let everyone connect.
+# Access control is handled below.
+config :peerdns, listen_api: [
+  {"0.0.0.0", standard_api_port},       # Anyone through IPv4
+  {"::", standard_api_port},            # Anyone through IPv6
+]
+
+# What hosts are allowed to use the privileged PeerDNS API
+# These peers will be allowed to edit all the data files that have editable: true
+config :peerdns, privileged_api_hosts: ["127.0.0.1", "::1"]
+
+# The sources of data for our name database
+config :peerdns, sources: [
+  [
+    name: "My domains",
+    file: "data/name_list.json",
+    editable: true,
+    weight: 1,
+    ping_to: [{peer_fly11, standard_api_port}],
+    ping_interval: 3600*12,   # 12 hours
+  ],
+  [
+    name: "My modlist",
+    file: "data/name_mod_list.json",
+    editable: true,
+    weight: 0.8,
+  ],
+]
+
+config :peerdns, ping: [
+  accept: true,
+  max_interval: 3600*24,    # one day
+  endorse_after: 3600*24*7, # one week
+  weight: 0.6
+]
+
+config :peerdns, neighbors: [
+  # Use these peers as neighbors in all cases
+  {:static, [
+    [host: {peer_fly11, standard_api_port}, weight: 0.9, push: true, pull: true]
+  ]},
+  # A list of peers to use as neighbors that can be modified through the API
+  {:file_db, [
+    file: "data/neighbors.json",
+    editable: "true",
+  ]},
+  # Look up our CJDNS neighbors by connecting to local cjdroute and try to
+  # use them as neighbors
+  {:cjdns, [port: standard_api_port, weight: 0.5, push: true, pull: true]}
+]
+
+config :peerdns, open: :accept
+config :peerdns, open_weight: 0.1
+
+config :peerdns, cutoff: 0.05
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
